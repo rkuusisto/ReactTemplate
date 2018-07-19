0a7ea11677abc567ca73c7514ae5bdd58a525854
Riku Kuusisto <riku-hermanni.kuusisto@elinar.fi>
2018-07-19 06:31:20 +0300

    Added possibility to hide the alert

--- Template-web/src/main/app/Components/Pages/ExamplePage.js
+++ Template-web/src/main/app/Components/Pages/ExamplePage.js
@@ -1,56 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import  ExampleDialog  from './../Dialogs/ExampleDialog'
+import ExampleDialog from './../Dialogs/ExampleDialog'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Alert } from 'reactstrap';
+import {Alert} from 'reactstrap';
 
 class ExamplePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            alertOpen:false,
-            alertStyle:'primary',
-            alertText:''
-        }
+            alertOpen: false,
+            alertStyle: 'primary',
+            alertText: ''
+        };
         this.displaySuccess = this.displaySuccess.bind(this);
         this.displayFailture = this.displayFailture.bind(this);
+        this.hideAlert = this.hideAlert.bind(this);
     }
 
     displaySuccess(data) {
-        this.setState( 
+        this.setState(
             {
                 alertOpen: true,
-                alertStyle:'primary',
+                alertStyle: 'primary',
                 alertText: data
             }
-    );
+        );
 
     }
 
     displayFailture(data) {
-        this.setState( 
-        {
-            alertOpen: true,
-            alertStyle:'danger',
-            alertText: data
+        this.setState(
+            {
+                alertOpen: true,
+                alertStyle: 'danger',
+                alertText: data
+            });
+    }
+
+    hideAlert() {
+        this.setState({
+            alertOpen: false,
+            alertStyle: 'primary',
+            alertText: ''
         });
     }
 
 
     render() {
-        return ( 
-          <div> 
-                <Alert color={this.state.alertStyle} isOpen={this.state.alertOpen}>
-                   {this.state.alertText}
+        return (
+            <div>
+                <Alert color={this.state.alertStyle} isOpen={this.state.alertOpen} toggle={this.hideAlert}>
+                    {this.state.alertText}
                 </Alert>
 
-              <ExampleDialog successCallBack={this.displaySuccess} failtureCallBack={this.displayFailture} />      
-          </div>
+                <ExampleDialog successCallBack={this.displaySuccess} failtureCallBack={this.displayFailture}/>
+            </div>
         );
     }
 };
 
 export default ExamplePage;
- 
+
 const wrapper = document.getElementById("index");
-wrapper ? ReactDOM.render(<ExamplePage />, wrapper) : false;
+wrapper ? ReactDOM.render(<ExamplePage/>, wrapper) : false;
